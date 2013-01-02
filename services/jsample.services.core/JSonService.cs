dd5bd982463ca87f199656932e0b0be0cf495b04
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-01-02 14:47:03 +0100

    services package

---
+++ services/jsample.services.core/JSonService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using jsample.api.core;
+
+namespace jsample.services.core
+{
+  public class JSonService : IApiService
+  {
+    public void ReadText(string text)
+    {
+      JsonReader r = new JsonTextReader(new StringReader(text));
+      r.Close();
+    }
+  }
+}
